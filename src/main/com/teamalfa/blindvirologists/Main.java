644be2ed05ae448d42b3bbcc4a45e53942249e30
fay-ambrus <ambrus.fay@gmail.com>
2022-04-18 21:49:47 +0200

    input lang initial

--- src/main/com/teamalfa/blindvirologists/Main.java
+++ src/main/com/teamalfa/blindvirologists/Main.java
@@ -1,8 +1,33 @@
 package main.com.teamalfa.blindvirologists;
-//Controller class
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        AController ac = new AController();
-        ac.runTests();
+        Controller controller = new Controller();
+
+        System.out.println("Would you like to play the game or run tests?");
+        System.out.println("1. Play Game");
+        System.out.println("2. Run Tests");
+
+        Scanner inputScanner = new Scanner(System.in);
+        while (true) {
+            try {
+                int choice = Integer.parseInt(inputScanner.nextLine());
+                if (choice == 1) {
+                    controller.playGame();
+                    return;
+                }
+                if (choice == 2) {
+                    controller.runTest();
+                    return;
+                } else {
+                    throw new NumberFormatException("Invalid input!");
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Incorrect number format! Enter either '1' or '2'!");
+            }
+        }
+
     }
 }
