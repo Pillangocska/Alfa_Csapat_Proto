9ce3313890ca5265cc7f8a68c9b5657077bccdce
András Jankó <andras.janko@edu.bme.hu>
2022-04-19 21:59:43 +0200

    XyCodes

--- src/main/com/teamalfa/blindvirologists/agents/genetic_code/GeneticCode.java
+++ src/main/com/teamalfa/blindvirologists/agents/genetic_code/GeneticCode.java
@@ -1,6 +1,7 @@
 package main.com.teamalfa.blindvirologists.agents.genetic_code;
 import main.com.teamalfa.blindvirologists.agents.Vaccine;
 import main.com.teamalfa.blindvirologists.agents.virus.Virus;
+import main.com.teamalfa.blindvirologists.virologist.Virologist;
 import main.com.teamalfa.blindvirologists.virologist.backpack.ElementBank;
 
 /**
@@ -10,4 +11,5 @@ import main.com.teamalfa.blindvirologists.virologist.backpack.ElementBank;
 abstract public class GeneticCode {
     abstract public Virus createVirus(ElementBank elementBank);
     abstract public Vaccine createVaccine(ElementBank elementBank);
+    public void autoInfect(Virologist virologist){  }
 }
