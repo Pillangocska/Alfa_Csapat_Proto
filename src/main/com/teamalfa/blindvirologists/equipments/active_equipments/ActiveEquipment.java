ff5f14ed9c97759e096576d4995c091a502ef212
András Jankó <andras.janko@edu.bme.hu>
2022-04-19 22:48:59 +0200

    Equipment

--- src/main/com/teamalfa/blindvirologists/equipments/active_equipments/ActiveEquipment.java
+++ src/main/com/teamalfa/blindvirologists/equipments/active_equipments/ActiveEquipment.java
@@ -4,28 +4,24 @@ import main.com.teamalfa.blindvirologists.equipments.Equipment;
 import main.com.teamalfa.blindvirologists.virologist.Virologist;
 
 abstract public class ActiveEquipment extends Equipment {
-    //private int cooldownDuration;
-    //private int cooldown = 0;
+    protected int cooldownDuration;
+    protected int cooldown;
+    protected int usetime;
 
     public abstract void use(Virologist v);
 
     public void equip(){
-        AController.printCall(this, "equip", null);
         virologist.addWorn(this);
         virologist.addActive(this);
-        AController.printReturn(null);
     }
 
     public void unEquip(){
-        AController.printCall(this, "unEquip", null);
         virologist.removeWorn(this);
         virologist.removeActive(this);
-        AController.printReturn(null);
     }
 
     public void startCooldown() {
-        AController.printCall(this, "startCooldown", null);
-        AController.printReturn(null);
+        //todo
     }
 
 }
