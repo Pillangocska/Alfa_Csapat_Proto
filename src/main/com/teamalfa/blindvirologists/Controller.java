03b957e5215aaac4cf05d03f0fe589f72bc09517
Lídia Tarcza <100163235+diatrcz@users.noreply.github.com>
2022-04-21 23:26:30 +0200

    uwu 5.0 not hehehe

--- src/main/com/teamalfa/blindvirologists/Controller.java
+++ src/main/com/teamalfa/blindvirologists/Controller.java
@@ -450,7 +450,7 @@ public class Controller {
         System.out.println();
     }
 
-    // EZT NEM ÉRTEM, HOGY HOGY KÉNE MŰKÖDNIE!
+    // EZT NEM ÉRTEM, HOGY HOGY KÉNE MŰKÖDNIE! todo
     public void setrandom(String outcome, int choiceType) {
         System.out.println(outcome);
         System.out.println(choiceType);
