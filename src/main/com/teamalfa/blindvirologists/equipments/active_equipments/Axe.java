ccb0797953ee587d18b5ee5ec7e36313364c8939
Fejes Kristóf <kristi199908@gmail.com>
2022-04-21 23:31:52 +0200

    virus changes

--- src/main/com/teamalfa/blindvirologists/equipments/active_equipments/Axe.java
+++ src/main/com/teamalfa/blindvirologists/equipments/active_equipments/Axe.java
@@ -3,12 +3,18 @@ package main.com.teamalfa.blindvirologists.equipments.active_equipments;
 import main.com.teamalfa.blindvirologists.virologist.Virologist;
 
 public class Axe extends ActiveEquipment {
+
     public Axe() {
 
     }
 
     @Override
     public void use(Virologist v) {
+
+    }
+
+    @Override
+    public void wornOut() {
 
     }
 }
