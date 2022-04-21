b9bca1dec5cdf78134e08a70f9ed0c2043e2f64a
Lídia Tarcza <100163235+diatrcz@users.noreply.github.com>
2022-04-21 21:49:57 +0200

    BearVirus

--- src/main/com/teamalfa/blindvirologists/agents/virus/BearVirus.java
+++ src/main/com/teamalfa/blindvirologists/agents/virus/BearVirus.java
@@ -5,6 +5,11 @@ import main.com.teamalfa.blindvirologists.virologist.Virologist;
 
 public class BearVirus extends DanceVirus{
 
+    /**
+     * This method infects the Virologist,
+     * if the infection was successful the Virologist is turned into a bear.
+     * @param target The Virologist the Virus was used on.
+     */
     @Override
     public void apply(Virologist target) {
         if(target.infectedBy(this)) {
